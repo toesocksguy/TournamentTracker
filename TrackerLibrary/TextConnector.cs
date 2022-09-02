b419809a536d2b72954fdf4641ed6ded87d69ed8
toesocksguy <toesocksguy@proton.me>
2022-09-02 14:05:58 -0700

    Create SQLConnector class (inherits from IDataConnection). Create TextConnector class (inherits from IDataConnection).

---
+++ TrackerLibrary/TextConnector.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerLibrary
+{
+    public class TextConnector : IDataConnection
+    {
+        // TODO - Make CreatePrize method actually save to text file
+        /// <summary>
+        /// Save a new prize to a text file.
+        /// </summary>
+        /// <param name="model">The prize info.</param>
+        /// <returns>The prize info, including the unique ID.</returns>
+        public PrizeModel CreatePrize(PrizeModel model)
+        {
+            model.Id = 1;
+
+            return model;
+        }
+    }
+}
