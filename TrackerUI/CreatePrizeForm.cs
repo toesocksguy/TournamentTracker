601654cf25e8d16ad9334469f5e2ae0e0f9b1e20
toesocksguy <toesocksguy@proton.me>
2022-09-13 12:08:11 -0700

    Reset text fields in CreatePrizeForm on createPrizeButton_Click()

--- TrackerUI/CreatePrizeForm.cs
+++ TrackerUI/CreatePrizeForm.cs
@@ -32,6 +32,11 @@ namespace TrackerUI
                 {
                     db.CreatePrize(model);
                 }
+
+                placeNameValue.Text = "";
+                placeNumberValue.Text = "";
+                prizeAmountValue.Text = "0";
+                prizePercentageValue.Text = "0";
             }
         }
 
