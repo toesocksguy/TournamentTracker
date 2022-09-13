3ce089c6cba80c3fb483b6e0d2fa3c0065f1632d
toesocksguy <toesocksguy@proton.me>
2022-09-13 12:55:10 -0700

    Add DataAccess and Models folders to TrackerLibrary for better structure

--- TrackerLibrary/Models/MatchupModel.cs
+++ TrackerLibrary/Models/MatchupModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.Models
 {
     public class MatchupModel
     {
@@ -12,12 +12,12 @@ namespace TrackerLibrary
         /// Represents the matchups
         /// </summary>
         public List<MatchupEntryModel> Entries { get; set; } = new List<MatchupEntryModel>();
-        
+
         /// <summary>
         /// Represents the winner of this match
         /// </summary>
         public TeamModel Winner { get; set; }
-        
+
         /// <summary>
         /// Represents the round of this match
         /// </summary>
--- TrackerLibrary/Models/TournamentModel.cs
+++ TrackerLibrary/Models/TournamentModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TrackerLibrary
+namespace TrackerLibrary.Models
 {
     public class TournamentModel
     {
@@ -12,22 +12,22 @@ namespace TrackerLibrary
         /// Represents this tournament
         /// </summary>
         public string TournamentName { get; set; }
-        
+
         /// <summary>
         /// Represents the entry fee for this tourament
         /// </summary>
         public decimal EntryFee { get; set; }
-        
+
         /// <summary>
         /// Represents the teams that are entered into this tournament
         /// </summary>
         public List<TeamModel> EnteredTeams { get; set; } = new List<TeamModel>();
-        
+
         /// <summary>
         /// Represents the prizes that are available in this tournament
         /// </summary>
         public List<PrizeModel> Prizes { get; set; } = new List<PrizeModel>();
-        
+
         /// <summary>
         /// Represents the matchups in this tournament
         /// </summary>
